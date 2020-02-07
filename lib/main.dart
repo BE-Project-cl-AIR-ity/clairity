b370be76bb55ae648181830f0b698c7eb092b98f
swaraj31 <Github@OP5>
2020-02-07 14:44:40 +0530

    Orange colour

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'branch'),
+      home: MyHomePage(title: 'cl-AIR-ity'),
     );
   }
 }
