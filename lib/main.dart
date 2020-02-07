2496f2ccab3ee4326f8f1dc4f75f80288a3579cb
rutuja2197 <rutuja.deshpande97@gmail.com>
2020-02-07 09:01:36 +0530

    added hyphens

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'branch_dhruv clairity123'),
+      home: MyHomePage(title: 'branch_dhruv '),
     );
   }
 }
