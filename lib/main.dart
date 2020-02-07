2806a7f7dbb9c8a6e32e172fb70b0995b588f1e7
swaraj31 <Github@OP5>
2020-02-07 14:09:20 +0530

    TRIAL dhruv 1234

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Clairity1234'),
+      home: MyHomePage(title: 'ClairityDhruv'),
     );
   }
 }
