971cf34b0519bbe71b8855f43c348f709a1ce204
swaraj31 <Github@OP5>
2020-02-11 19:23:06 +0530

    Trail swaraj123 branch

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Clairity I am done with GIT'),
+      home: MyHomePage(title: 'Clairity I am not done with GIT'),
     );
   }
 }
